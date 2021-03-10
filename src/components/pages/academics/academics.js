af26b29554ef05463d84243db2e6c7dfce0852c3
CandyPanda-LS <it19132310@my.sliit.lk>
2021-03-10 10:05:45 +0530

    2nd commit

--- src/components/pages/academics/academics.js
+++ src/components/pages/academics/academics.js
@@ -1,10 +1,15 @@
 import React ,{useEffect}from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import "./academics.css"
 
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
+
+import * as FaIcons from 'react-icons/fa';
+import * as BsIcons from 'react-icons/bs';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -29,9 +34,127 @@ function Academics() {
         return (
             <div data-aos="fade-in">
                 <div className={classes.root}>
-                  <Paper elevation={0}>
-                    <h1>Academics</h1>
-                  </Paper>
+                    <Paper elevation={0}>
+                        <h1>Academics</h1>
+
+                        <br/>
+                <br/>
+
+                <p>
+                  I'm currently a second year undergraduate at University of Colombo, Faculty of science.I'm also a third year undergraduate at Sri Lanka Institute of Information technology(SLIIT).
+                  I like to walk the mesterious path of the unexplored sciences and challenge myself to the limits. I am also a tech enthusist exploring Machine learning and IOT. I'm also a fullstack developer  
+                  with the MERN stack as my forte. I'm a team player, a charismatic leader and a dedicated personality who beleives in hard work paying off. I'm 
+                  also a volunteer who beleives in paying it foward.
+                </p>
+                <br/>
+                <br/>
+                <div className="divContainer2">
+                  <div className="div12">
+                  <div>
+                    <div className="infoBox2" data-aos="flip-down" data-aos-duration="4000">
+
+                      <div className="infoBoxIcon">
+                        <FaIcons.FaUserGraduate />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                              Degree(Reading)
+                            </h1>
+                        </div>
+                        <div className="infoBoxdespription">
+                            <p>
+                              Physical Science. Univertity of Colombo,Faculty of Science.
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                      <div className="infoBox2" data-aos="flip-down" data-aos-duration="4000">
+                        <div className="infoBoxIcon">
+                        <FaIcons.FaUserGraduate />
+                        </div>
+                        <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                              Degree(Reading)
+                            </h1>
+                        </div>
+                        <div className="infoBoxdespription">
+                            <p>
+                              Software Engineering. Srilanka Institute of Information Technology.
+                            </p>
+                        </div>
+                        </div>
+                      </div>
+
+                    
+                      <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                        <BsIcons.BsBook />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Richmond College(2005-2018)
+                            </h1>
+                        </div>
+                        <div className="infoBoxdespription">
+                            <p>
+                              Primary / Secondary education
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    
+                    
+                    </div>
+                  </div>
+
+                  <div className="div22">
+
+                    
+                    
+                    <div className="infoBox2" data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                        <BsIcons.BsBook />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                              Ordinary Level Examination(2015)
+                            </h1>
+                        </div>
+                        <div className="infoBoxdespription">
+                            <p>
+                                A-8 / B-1
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                    
+                    <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                        <BsIcons.BsBook />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Advanced level examination(2018)
+                            </h1>
+                        </div>
+                        <div className="infoBoxdespription">
+                            <p>
+                              B-3(Physical Science stream)
+                            </p>
+                        </div>
+                        </div>
+                    </div>
+                  </div>
+                
+                </div>
+                    </Paper>
+                  
+
                 </div>
             </div>
         )
--- src/components/pages/experience/Experience.js
+++ src/components/pages/experience/Experience.js
@@ -6,6 +6,9 @@ import Paper from '@material-ui/core/Paper';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+import * as BsIcons from 'react-icons/bs';
+
+
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
@@ -21,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
+
 function Experience() {
   useEffect(() => {
     AOS.init({duration :2000});
@@ -33,6 +37,195 @@ function Experience() {
             <div className={classes.root}>
               <Paper elevation={0}>
                 <h1>Experience</h1>
+                <br/>
+                <br/>
+                <h3>
+                  AIESEC
+                </h3>
+                <br/>
+                <br/>
+                <p>
+                  AIESEC is a non political, non-for profit , youth governed organization which helps young people develop their leadership skills. I was privillaged to be a part of 
+                  the membership at AIESEC in SLIIT and take part in many projects that help make the world a better place. 
+                </p>
+                <br/>
+                <br/>
+                <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/AIESEClogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Vice president - External Relations , Catalyst 2020
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+
+                            <p>
+                              I was the vice president - external relations for catalyst 2020. The first ever youth conference organized by AIESEC in SLIIT.
+                              In here I had the privillage of working with a wonderful team an improve my network and meet many young volunteers who are passionate 
+                              about changing the world.
+                            </p>
+                        </div>
+                        </div>
+                  </div>
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/AIESEClogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Manager - Partership Development , AIESEC in SLIIT 20/21
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+
+                            <p>
+                              I was a manager for partnership development at AIESEC in SLIIT for the term 20/21. There I got the experience of approaching 
+                              many companies to partner up with AIESEC. I also had the privillage of making many contacts within the industry itself through this process.
+                            </p>
+                        </div>
+                        </div>
+                  </div>
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/AIESEClogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Team Leader - Incomming Global Entrepreneur , AIESEC in SLIIT 20/21
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+
+                            <p>
+                              I was privillaged to hold the team leader position for the Incomming Global Entrepreneur function for AIESEC in SLIIT for the term 20/21.
+                              Incomming Global Entrepreneur(IGE) is an exchange process that helps foreign students to do an internship in Srilanka while also getting the experience of the Srilankan culture.
+                              Although we weren't able to do any exchanges due to covid. I gained a lot of experience by working with my team in how to plan ad form stratergies
+                            </p>
+                        </div>
+                        </div>
+                  </div>
+
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/AIESEClogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Team member - project 'Minisa'
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+
+                            <p>
+                              I was able to be a part of the team that organized the 8th edition of project 'Minisa' which helps uplift the lifestyle of kids suffering from down syndrome in Srilanka
+                            </p>
+                        </div>
+                        </div>
+                  </div>
+                <br/>
+                <br/>
+                <h3>
+                  Youth Governs Srilanka(YGSL)
+                </h3>
+                <br/>
+                <br/>
+                <p>
+                  Youth Governs Srilanka is a youth led organization in Srilanka which gives professional services to fields of Academia, Industry, Research, Innovation and our general community.
+                  Here I got the opportunity to work with undergraduates from many different Universities in Srilanka who are passionate about volunteering 
+                </p>
+                <br/>
+                <br/>
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/YGSL.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Member - Team Engineering and Technology
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+                            <p>
+                              I was a member of the Engeering and Technology branch of YGSL and engaged in many projects. I was also in the bi-weekly blog team Artigos
+                            </p>
+                        </div>
+                        </div>
+                  </div>     
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/YGSL.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Member - IDLE team.
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+                            <p>
+                              I was privillaged to be a part of team IDLE which organized a python workshop series for university undergraduates to improve their programming knowledge.
+                            </p>
+                        </div>
+                        </div>
+                  </div>
+
+                  <br/>
+                <br/>
+                <h3>
+                  Microsoft Club of SLIIT
+                </h3>
+ 
+                <br/>
+                <br/>
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/MSlogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                Member - Dev team
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+                            <p>
+                              I am a part of the developer team at MS club
+                            </p>
+                        </div>
+                        </div>
+                  </div>     
+                  <div className="infoBox2"data-aos="flip-down" data-aos-duration="4000">
+                      <div className="infoBoxIcon" >
+                      <img src={process.env.PUBLIC_URL + "/assets/MSlogo.png"} />
+                      </div>
+                      <div className="infoBoxContent">
+                        <div className="infoBoxHeading">
+                            <h1>
+                                OC - Return[C]ero
+                            </h1>
+                        </div>
+                        <br/>
+                        <div className="infoBoxdespription">
+                            <p>
+                              I was in the organizing committee of 'Return[C]ero' an introductory session to C programming. Me and my fellow colegues did 4 sessions in this programs covering different parts to fundermentals of programming
+                            </p>
+                        </div>
+                        </div>
+                  </div>     
               </Paper>
             </div>
         </div>
